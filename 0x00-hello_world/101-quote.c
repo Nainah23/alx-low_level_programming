05f5739443a78d75a731b4b4de78fbb1837b77e5
Nainah23 <kamauwainaina29@gmail.com>
2023-06-15 14:10:14 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (error)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful"
-		       "Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "This is a string";
+	fwrite(str, sizeof(char), strlen(str), stderr);
 	return (1);
 }

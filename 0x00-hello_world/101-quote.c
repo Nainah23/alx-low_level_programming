8f6d8e06b7e7b4afcdb419f1d6ed848988e44fff
Nainah23 <kamauwainaina29@gmail.com>
2023-06-15 14:41:05 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 #include <string.h>
 
 /**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	char str[] = "This is a string";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
 	fwrite(str, sizeof(char), strlen(str), stderr);
 	return (1);

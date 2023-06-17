9258f9b2a302cf519a7bb5175d982d023f4986ab
Nainah23 <kamauwainaina29@gmail.com>
2023-06-17 10:08:16 -0700

    q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return:0 Success
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

75b73ef076232e6c296e49133f7b3bdb4eb924c4
Nainah23 <kamauwainaina29@gmail.com>
2023-06-20 13:10:37 -0700

    z

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,12 +16,17 @@ void times_table(void)
 	{
 		_putchar(',');
 		_putchar(' ');
+
 		result = digit * mult;
-	if (result <= 9)
+
+		if (result <= 9)
 		_putchar(' ');
-	else
-		_putchar((result / 10) + '0');
+		else
+			_putchar((result / 10) + '0');
+
+		_putchar((result % 10) + '0');
 	}
+
 	_putchar('\n');
 	}
 }

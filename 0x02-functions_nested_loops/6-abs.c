a39788097ac6ad8c2bf2364c090d331b8419de19
Nainah23 <kamauwainaina29@gmail.com>
2023-06-20 12:23:05 -0700

    z

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - prints a number's absolute value
+ *@i: number targeted
+ *Return: returns 0
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (0);
+	}
+}

90f6890ef409f1e4c7d150e74f0cf703febe9f60
Nainah23 <kamauwainaina29@gmail.com>
2024-04-10 01:56:02 -0700

    Final Commit

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+  * recursive_advanced_binary - Searches recursively for a value in a sorted
+  *                             array of integers using binary search algorithm.
+  * @array: A pointer to the first element of the [sub]array to search in.
+  * @left: The starting index of the [sub]array to search in.
+  * @right: The ending index of the [sub]array to search in.
+  * @value: The value to search for.
+  *
+  * Return: The index where the value is located
+  * 	or -1 If the value is not present.
+  *
+  */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
+}
+
+/**
+  * advanced_binary - Searches for a value in a sorted array
+  *                   of integers using advanced Binary search algo.
+  * @array: A pointer to the first element of the array to search in.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: The index where the value is located
+  * or -1 If the value is not present.
+  * Description: Every time you split the array, you have to print the new
+  * 	array (or subarray) you’re searching in
+  */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using Linear skip algorithm.
+ * @list: A pointer to the  head of the linked list to search in.
+ * @value: The value to search for.
+ *
+ * Return: A pointer to the first node where the value is located
+ * 	or NULL If the value is not present or the head of the list is NULL.
+ *
+ * Description: Every time you compare a value in the list to the value you
+ * 	are searching, you have to print this value.
+ * 	Uses the square root of the list size as the jump step.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
